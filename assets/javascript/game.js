268e03e6324e290a8ecacb6aa41ff6f98f8df7e9
xtianares <xtianares@yahoo.com>
2018-12-26 15:55:07 -0500

    initial game setup

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// trivia questions, q = questions, c = choices and a = index of correct choice
+const questions = [
+    {
+        q: "What is this game?",
+        c: ["Trivia Game", "I don't know", "Something Else", "I Need Help"],
+        a: 0,
+    },
+    {
+        q: "What is this game 2?",
+        c: [ "I don't know", "Something Else", "Trivia Game", "I Need Help"],
+        a: 2,
+    }
+];
+
+let currentQuestion = 0,
