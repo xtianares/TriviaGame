ec85e2fe9c45d4bb81c9785ba4c422159aff5810
xtianares <xtianares@yahoo.com>
2018-12-27 14:01:36 -0500

    working timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,41 @@
 // trivia questions, q = questions, c = choices and a = index of correct choice
 const questions = [
     {
-        q: "What is this game?",
-        c: ["Trivia Game", "I don't know", "Something Else", "I Need Help"],
+        q: "What ingredient in fresh milk is eventually devoured by bacteria, causing the sour taste?",
+        c: ["Lactose", "Conola Oil", "Cocoa Powder", "Calcium"],
         a: 0,
     },
     {
-        q: "What is this game 2?",
-        c: [ "I don't know", "Something Else", "Trivia Game", "I Need Help"],
+        q: "What type of animal is a skink?",
+        c: [ "Dog", "Cat", "Dragon", "Lizard"],
         a: 2,
     }
 ];
+
+let currentQuestionIndex = getRandomNumber(0, questions.length),
+    currentQuston = questions[currentQuestionIndex],
+    timer = 15,
+    timerId;
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-let currentQuestionIndex = getRandomNumber(0, questions.length),
-    currentQuston = questions[currentQuestionIndex];
+function timerInit() {
+    $(".timer").html(timer);
+    clearInterval(timerId);
+    timerId = setInterval(decrement, 1000);
+}
+function decrement() {
+    timer--;
+    //  Show the number in the #show-number tag.
+    $(".timer").html(timer);
+    //  Once number hits zero...
+    if (timer === 0) {
+        timerStop();
+    }
+}
+function timerStop() {
+    clearInterval(timerId);
+}
+timerInit();
