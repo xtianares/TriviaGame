e464de2bec9f526100b36312707e699e5e6e2fe3
xtianares <xtianares@yahoo.com>
2018-12-27 13:44:01 -0500

    css and hmtl setup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,4 +12,9 @@ const questions = [
     }
 ];
 
-let currentQuestion = 0,
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+let currentQuestionIndex = getRandomNumber(0, questions.length),
+    currentQuston = questions[currentQuestionIndex];
