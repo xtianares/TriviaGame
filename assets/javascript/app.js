d79f48bed2bcc95fa94940cecd72d553b5221ff1
xtianares <xtianares@yahoo.com>
2018-12-29 00:34:32 -0500

    game logic and display done, still need to pretty it up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -63,26 +63,40 @@ function decrement() {
     if (runningTimer === 0) {
         timerStop();
         $(".choices").html('<div class="times-up"><h3>Times Up!</h3><p>Correct answer is: ' + questions[questionIndex].a + '<p></div>');
-        if (shownQuestionIndex.length < questions.length) {
-            setTimeout(init, 3000);
-        }
-        else {
-            // show game summary function and restart option
-            console.log("game over");
-        }
+        nextQuestion();
     }
 }
 function timerStop() {
     clearInterval(timerId);
 }
 function checkAnswer(answer) {
+    timerStop();
     if (answer === questions[questionIndex].a) {
         correctAnswers++;
-        console.log('correctAnswers: ' + correctAnswers);
+        // console.log('correctAnswers: ' + correctAnswers);
+        $(".choices").html('<div class="times-up"><h3>You got it!</h3><p>Correct answer is: ' + questions[questionIndex].a + '<p></div>');
+        nextQuestion();
     }
     else {
         wrongAnswers++;
-        console.log('wrongAnswers: ' + wrongAnswers);
+        // console.log('wrongAnswers: ' + wrongAnswers);
+        $(".choices").html('<div class="result"><h3>Wrong!</h3><p>Correct answer is: ' + questions[questionIndex].a + '<p></div>');
+        nextQuestion();
+    }
+}
+function nextQuestion() {
+    if (shownQuestionIndex.length < questions.length) {
+        setTimeout(init, 3000);
+    }
+    else {
+        // show game summary function and restart option
+        setTimeout(function() {
+            console.log("game over");
+            $('.status-area').hide();
+            $(".question").replaceWith('<h3 class="question">Game Summary</h3>');
+            $(".choices").html('<div class="result"><h4>Correct Answers: ' + correctAnswers + '</h4><h4>Wrong Answers: ' + wrongAnswers + '</h4></div>');
+            $(".choices").after('<button class="restart-btn btn btn-success">Restart the game!</button>');
+        }, 3000);
     }
 }
 function restart() {
@@ -90,10 +104,15 @@ function restart() {
     correctAnswers = 0,
     wrongAnswers = 0;
     init();
+    $('.status-area').show();
+    $('.restart-btn').remove();
 }
 
-$('.start-area').click(function() {
+$(document).on('click', '.start-btn', function() {
     init();
+});
+$(document).on('click', '.restart-btn', function() {
+    restart();
 });
 $(document).on('click', '.choices .answer', function() {
     let answer = $(this).text();
