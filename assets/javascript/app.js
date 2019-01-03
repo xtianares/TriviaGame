c6a8c2034f2f17b743f655f2e26309cf38eef4d3
xtianares <xtianares@yahoo.com>
2019-01-02 23:46:46 -0500

    updated app.js, added audio

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,96 +8,101 @@ let questionIndex = 0,
     runningTimer = 0,
     timerId;
 
-function init() {
-    timerInit();
-    questionIndex = Math.floor(Math.random() * (questions.length)); // randomization of questions
-    currentQuestion = questions[questionIndex].q;
-    questionChoices = questions[questionIndex].c;
-    console.log(questionIndex, shownQuestionIndex.length, questions.length)
-    if (shownQuestionIndex.includes(questionIndex) && shownQuestionIndex.length < questions.length) {
-        init();
-    }
-    else {
-        shownQuestionIndex.push(questionIndex);
-        //console.log(questionIndex, currentQuestion, shownQuestionIndex)
-        $('.game-row').css('display', 'flex');
-        $('.start-area').hide();
-        $('.number').text(shownQuestionIndex.length); // show current number of question
-        $('.total-questions').text(questions.length); // show total number of questions
-        $('.question').text(currentQuestion); // display current question
-        let choices = '';
-        questionChoices.sort(function() { return 0.5 - Math.random() }); // randomizing the answers from https://css-tricks.com/snippets/javascript/shuffle-array/
-        for (var i = 0; i < questionChoices.length; i++) {
-            choices += '<button class="btn btn-outline-success answer">' + questionChoices[i] + '</button>';
+const trivia = {
+    init: function() {
+        trivia.timerInit();
+        questionIndex = Math.floor(Math.random() * (questions.length)); // randomization of questions
+        currentQuestion = questions[questionIndex].q;
+        questionChoices = questions[questionIndex].c;
+        // console.log(questionIndex, shownQuestionIndex.length, questions.length)
+        if (shownQuestionIndex.includes(questionIndex) && shownQuestionIndex.length < questions.length) {
+            trivia.init();
         }
-        $('.choices').html(choices); // display current question's choices
+        else {
+            shownQuestionIndex.push(questionIndex);
+            // console.log(questionIndex, currentQuestion, shownQuestionIndex)
+            $('.game-row').css('display', 'flex');
+            $('.start-area').hide();
+            $('.number').text(shownQuestionIndex.length); // show current number of question
+            $('.total-questions').text(questions.length); // show total number of questions
+            $('.question').text(currentQuestion); // display current question
+            let choices = '';
+            questionChoices.sort(function() { return 0.5 - Math.random() }); // randomizing the answers from https://css-tricks.com/snippets/javascript/shuffle-array/
+            for (var i = 0; i < questionChoices.length; i++) {
+                choices += '<button class="btn btn-outline-success answer">' + questionChoices[i] + '</button>';
+            }
+            $('.choices').html(choices); // display current question's choices
+        }
+    },
+    timerInit: function() {
+        runningTimer = setTimer,
+        $(".timer").text(runningTimer);
+        clearInterval(timerId);
+        timerId = setInterval(trivia.decrement, 1000);
+    },
+    decrement: function() {
+        runningTimer--;
+        $(".timer").text(runningTimer);
+        if (runningTimer === 0) {
+            trivia.timerStop();
+            wrongAnswers++;
+            $(".choices").html('<div class="times-up"><h3>Times Up!</h3><p>Correct answer is: ' + questions[questionIndex].a + '<p></div>');
+            trivia.nextQuestion();
+        }
+    },
+    timerStop: function() {
+        clearInterval(timerId);
+    },
+    checkAnswer: function(answer) {
+        this.timerStop();
+        if (answer === questions[questionIndex].a) {
+            correctAnswers++;
+            // console.log('correctAnswers: ' + correctAnswers);
+            $(".choices").html('<div class="times-up"><h3 class="h4">You got it!</h3><p>Correct answer is:<span class="correct-answer">' + questions[questionIndex].a + '</span><p></div>');
+            this.nextQuestion();
+        }
+        else {
+            wrongAnswers++;
+            // console.log('wrongAnswers: ' + wrongAnswers);
+            $(".choices").html('<div class="result"><h3 class="h4">Wrong!</h3><p>Correct answer is:<span class="correct-answer">' + questions[questionIndex].a + '</span><p></div>');
+            this.nextQuestion();
+        }
+    },
+    nextQuestion: function() {
+        if (shownQuestionIndex.length < questions.length) {
+            setTimeout(trivia.init, 2000);
+        }
+        else {
+            // show game summary function and restart option
+            setTimeout(function() {
+                console.log("game over");
+                $('.status-area').hide();
+                $(".question").replaceWith('<h3 class="question">Game Summary</h3>');
+                $(".choices").html('<div class="result"><h4>Correct Answers: ' + correctAnswers + '</h4><h4>Wrong Answers: ' + wrongAnswers + '</h4></div>');
+                $(".choices").after('<button class="restart-btn btn btn-success">Restart the game!</button>');
+            }, 2000);
+        }
+    },
+    restart: function() {
+        shownQuestionIndex = [],
+        correctAnswers = 0,
+        wrongAnswers = 0;
+        this.init();
+        $('.status-area').show();
+        $('.restart-btn').remove();
     }
-}
-function timerInit() {
-    runningTimer = setTimer,
-    $(".timer").text(runningTimer);
-    clearInterval(timerId);
-    timerId = setInterval(decrement, 1000);
-}
-function decrement() {
-    runningTimer--;
-    $(".timer").text(runningTimer);
-    if (runningTimer === 0) {
-        timerStop();
-        wrongAnswers++;
-        $(".choices").html('<div class="times-up"><h3>Times Up!</h3><p>Correct answer is: ' + questions[questionIndex].a + '<p></div>');
-        nextQuestion();
-    }
-}
-function timerStop() {
-    clearInterval(timerId);
-}
-function checkAnswer(answer) {
-    timerStop();
-    if (answer === questions[questionIndex].a) {
-        correctAnswers++;
-        // console.log('correctAnswers: ' + correctAnswers);
-        $(".choices").html('<div class="times-up"><h3 class="h4">You got it!</h3><p>Correct answer is:<span class="correct-answer">' + questions[questionIndex].a + '</span><p></div>');
-        nextQuestion();
-    }
-    else {
-        wrongAnswers++;
-        // console.log('wrongAnswers: ' + wrongAnswers);
-        $(".choices").html('<div class="result"><h3 class="h4">Wrong!</h3><p>Correct answer is:<span class="correct-answer">' + questions[questionIndex].a + '</span><p></div>');
-        nextQuestion();
-    }
-}
-function nextQuestion() {
-    if (shownQuestionIndex.length < questions.length) {
-        setTimeout(init, 2000);
-    }
-    else {
-        // show game summary function and restart option
-        setTimeout(function() {
-            console.log("game over");
-            $('.status-area').hide();
-            $(".question").replaceWith('<h3 class="question">Game Summary</h3>');
-            $(".choices").html('<div class="result"><h4>Correct Answers: ' + correctAnswers + '</h4><h4>Wrong Answers: ' + wrongAnswers + '</h4></div>');
-            $(".choices").after('<button class="restart-btn btn btn-success">Restart the game!</button>');
-        }, 2000);
-    }
-}
-function restart() {
-    shownQuestionIndex = [],
-    correctAnswers = 0,
-    wrongAnswers = 0;
-    init();
-    $('.status-area').show();
-    $('.restart-btn').remove();
 }
 
+
 $(document).on('click', '.start-btn', function() {
-    init();
+    trivia.init();
+    let audio = new Audio('assets/sounds/rainforest-ambience.mp3');
+    audio.play();
 });
 $(document).on('click', '.restart-btn', function() {
-    restart();
+    trivia.restart();
 });
 $(document).on('click', '.choices .answer', function() {
     let answer = $(this).text();
-    checkAnswer(answer);
+    trivia.checkAnswer(answer);
 });
